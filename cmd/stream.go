c3aa3cb895e713172fce850c7bcb2228e67eeec1
lesnuages <1799430+lesnuages@users.noreply.github.com>
2018-11-27 17:12:29 +0100

    Added command line support for stream

--- cmd/stream.go
+++ cmd/stream.go
@@ -3,29 +3,40 @@ package cmd
 import (
 	"encoding/hex"
 	"fmt"
+	"strconv"
 
 	"github.com/desertbit/grumble"
 	"github.com/fatih/color"
 )
 
 func stream(c *grumble.Context) error {
+	var (
+		sessionID uint64
+		err       error
+	)
 	if Parser.CurrentSession != 0 {
-		if session, ok := Parser.Sessions[Parser.CurrentSession]; ok {
-			for idx, packet := range session.Packets {
-				if len(packet.TransportLayer().LayerPayload()) != 0 {
-					header := color.New(color.FgCyan, color.Bold)
-					header.Printf("[Packet %d]\n", idx)
-					if c.Flags.Bool("string") {
-						fmt.Println(string(packet.TransportLayer().LayerPayload()[:]))
-					} else {
-						fmt.Println(hex.EncodeToString(packet.TransportLayer().LayerPayload()))
-					}
+		sessionID = Parser.CurrentSession
+	} else if len(c.Args) != 0 {
+		if sessionID, err = strconv.ParseUint(c.Args[0], 10, 64); err != nil {
+			return err
+		}
+	} else {
+		return fmt.Errorf("you need to select a session first")
+	}
+	if session, ok := Parser.Sessions[sessionID]; ok {
+		for idx, packet := range session.Packets {
+			if len(packet.TransportLayer().LayerPayload()) != 0 {
+				header := color.New(color.FgCyan, color.Bold)
+				header.Printf("[Packet %d]\n", idx)
+				if c.Flags.Bool("string") {
+					fmt.Println(string(packet.TransportLayer().LayerPayload()[:]))
+				} else {
+					fmt.Println(hex.EncodeToString(packet.TransportLayer().LayerPayload()))
 				}
 			}
 		}
-		return nil
 	}
-	return fmt.Errorf("you need to select a session first")
+	return nil
 }
 
 func init() {
@@ -36,7 +47,8 @@ func init() {
 			f.Bool("h", "hex", true, "Print as hexadecimal dump")
 			f.Bool("s", "string", false, "Print as ASCII string")
 		},
-		Run: stream,
+		AllowArgs: true,
+		Run:       stream,
 	}
 	App.AddCommand(stream)
 }
